8809456e6452c89d70ffe760a2775418832d20bc
danielzalukhu <33409087+danielzalukhu@users.noreply.github.com>
2020-08-27 22:14:11 +0700

    modified footer component. Create new name route is product to hande page of detail products at index.js. Create new views file is Product.vue

--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,35 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Product from '../views/Product.vue'
 
 Vue.use(VueRouter)
 
-  const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
-]
+  const routes = [{
+      path: '/',
+      name: 'Home',
+      component: Home
+    },
+    // Code bellow is the second choice to create some route using lazy-load 
+    // init the component inside the route installation code.
+    {
+      path: '/about',
+      name: 'About',
+      // route level code-splitting
+      // this generates a separate chunk (about.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    },
+    {
+      path: '/product',
+      name: 'Product',
+      component: Product
+    }
+  ]
 
 const router = new VueRouter({
   routes
 })
 
 export default router
+
---
+++ src/views/Product.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="product">
+    <HeaderShayna /> 
+    <p>
+        HAI SAYA BAGIAN PRODUK
+    </p>
+    <FooterShayna />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HeaderShayna from '@/components/HeaderShayna.vue';
+import FooterShayna from '@/components/FooterShayna.vue';
+
+export default {
+  name: 'Product',
+  components: {
+    HeaderShayna,
+    FooterShayna
+  }
+}
+</script>
