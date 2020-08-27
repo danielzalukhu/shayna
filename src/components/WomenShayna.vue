86a0180d61d60e80da87d210701351f069a35de4
danielzalukhu <33409087+danielzalukhu@users.noreply.github.com>
2020-08-27 11:42:58 +0700

    add new images product, init 2 components women and hero, modified views home

---
+++ src/components/WomenShayna.vue
@@ -0,0 +1,113 @@
+<template>
+    <section class="women-banner spad">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-lg-12 mt-5">
+                    <carousel class="product-slider" :items="3" :nav="false" :autoplay="true">
+                        <div class="product-item">
+                            <div class="pi-pic">
+                                <img src="img/bag.jpeg" alt="" />
+                                <ul>
+                                    <li class="w-icon active">
+                                        <a href="#"><i class="icon_bag_alt"></i></a>
+                                    </li>
+                                    <li class="quick-view"><a href="product.html">+ Quick View</a></li>
+                                </ul>
+                            </div>
+                            <div class="pi-text">
+                                <div class="catagory-name">Bag</div>
+                                <a href="#">
+                                    <h5>Exclusive Bag</h5>
+                                </a>
+                                <div class="product-price">
+                                    $56.00
+                                    <span>$115.00</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="product-item">
+                            <div class="pi-pic">
+                                <img src="img/pants.jpeg" alt="" />
+                                <ul>
+                                    <li class="w-icon active">
+                                        <a href="#"><i class="icon_bag_alt"></i></a>
+                                    </li>
+                                    <li class="quick-view"><a href="#">+ Quick View</a></li>
+                                </ul>
+                            </div>
+                            <div class="pi-text">
+                                <div class="catagory-name">Women Pants</div>
+                                <a href="#">
+                                    <h5>"Paper" Green Pants</h5>
+                                </a>
+                                <div class="product-price">
+                                    $57.00
+                                </div>
+                            </div>
+                        </div>
+                        <div class="product-item">
+                            <div class="pi-pic">
+                                <img src="img/shirt1.jpeg" alt="" />
+                                <ul>
+                                    <li class="w-icon active">
+                                        <a href="#"><i class="icon_bag_alt"></i></a>
+                                    </li>
+                                    <li class="quick-view"><a href="product.html">+ Quick View</a></li>
+                                </ul>
+                            </div>
+                            <div class="pi-text">
+                                <div class="catagory-name">T-Shirt</div>
+                                <a href="#">
+                                    <h5>White Shirt Combine With Outer Bra</h5>
+                                </a>
+                                <div class="product-price">
+                                    $34.00
+                                </div>
+                            </div>
+                        </div>
+                        <div class="product-item">
+                            <div class="pi-pic">
+                                <img src="img/mickey1.jpg" alt="" />
+                                <ul>
+                                    <li class="w-icon active">
+                                        <a href="#"><i class="icon_bag_alt"></i></a>
+                                    </li>
+                                    <li class="quick-view"><a href="#">+ Quick View</a></li>
+                                    <li class="w-icon">
+                                        <a href="#"><i class="fa fa-random"></i></a>
+                                    </li>
+                                </ul>
+                            </div>
+                            <div class="pi-text">
+                                <div class="catagory-name">Bag</div>
+                                <a href="#">
+                                    <h5>Micky Bag</h5>
+                                </a>
+                                <div class="product-price">
+                                    $50.00
+                                </div>
+                            </div>
+                        </div>
+                    </carousel>
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
+
+<script>
+import carousel from 'vue-owl-carousel';
+
+export default {
+    name: 'WomenShayna',
+    components: {
+        carousel
+    }
+}
+</script>
+
+<style scoped>
+    .product-item{
+        margin-right: 25px;
+    }
+</style>
